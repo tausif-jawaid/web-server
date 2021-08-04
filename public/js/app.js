eb350cb5707e20be920b4203a4ba86a0ccd0a42f
tausif-jawaid <tausifjawaid6@gmail.com>
2021-08-04 21:51:49 +0530

    Remove unnecessary console.log callback

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,10 @@
-console.log('Client side javascript is enable');
 
 fetch('http://puzzle.mead.io/puzzle').then((response) => {
       response.json().then((data) => {
             console.log(data);
       })
 })
-    
+
 
 const weatherFormData = document.querySelector('form');
 const search = document.querySelector('input');
